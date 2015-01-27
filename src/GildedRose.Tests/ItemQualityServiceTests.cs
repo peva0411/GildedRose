7b71f4cfb589f18bf74796a942dbd53c576bdfb2
Phil <peva0411@gmail.com>
2015-01-27 07:20:18 -0500

    added tests for tickets and sulfuras

--- src/GildedRose.Tests/ItemQualityServiceTests.cs
+++ src/GildedRose.Tests/ItemQualityServiceTests.cs
@@ -125,5 +125,119 @@ namespace GildedRose.Tests
             Assert.That(agedBrie.Quality, Is.EqualTo(startingQuality + 2));
         }
 
+        [Test]
+        public void UpdateItemQuality_Sulfuras_QualityDoesNotDecrease()
+        {
+            var qualityService = GetQualityService();
+
+            var sulfuras = ItemBuilder.DefaultItem()
+                .WithQualityOf(10)
+                .WithSellInOf(0)
+                .ItemAsSulfuras()
+                .Build();
+
+            var startingQuality = sulfuras.Quality;
+
+            qualityService.UpdateItemQuality(sulfuras);
+            
+            Assert.That(sulfuras.Quality, Is.EqualTo(startingQuality));
+        }
+
+        [Test]
+        public void UpdateItemQuality_Tickets11DaysBeforeConcert_QualityIncreasesBy1()
+        {
+            var qualityService = GetQualityService();
+
+            var tickets = ItemBuilder.DefaultItem()
+                .WithQualityOf(20)
+                .WithSellInOf(11)
+                .ItemAsBackstagePasses()
+                .Build();
+
+            var startingQuality = tickets.Quality;
+
+            qualityService.UpdateItemQuality(tickets);
+
+
+            Assert.That(tickets.Quality, Is.EqualTo(startingQuality + 1));
+        }
+
+
+        [Test]
+        public void UpdateItemQuality_Ticket10DaysBeforeConcert_QualityIncreasesBy2()
+        {
+            var qualityService = GetQualityService();
+
+            var tickets = ItemBuilder.DefaultItem()
+                .WithQualityOf(20)
+                .WithSellInOf(10)
+                .ItemAsBackstagePasses()
+                .Build();
+
+            var startingQuality = tickets.Quality;
+
+            qualityService.UpdateItemQuality(tickets);
+
+
+            Assert.That(tickets.Quality, Is.EqualTo(startingQuality + 2));
+        }
+
+        [Test]
+        public void UpdateItemQuality_Tickets6DaysBeforeConcert_QualityIncreasesBy2()
+        {
+            var qualityService = GetQualityService();
+
+            var tickets = ItemBuilder.DefaultItem()
+                .WithQualityOf(20)
+                .WithSellInOf(6)
+                .ItemAsBackstagePasses()
+                .Build();
+
+            var startingQuality = tickets.Quality;
+
+            qualityService.UpdateItemQuality(tickets);
+
+
+            Assert.That(tickets.Quality, Is.EqualTo(startingQuality + 2));
+        }
+
+        [Test]
+        public void UpdateItemQuality_Tickets5DaysBeforeConcert_QualityIncreasesBy3()
+        {
+            var qualityService = GetQualityService();
+
+            var tickets = ItemBuilder.DefaultItem()
+                .WithQualityOf(20)
+                .WithSellInOf(5)
+                .ItemAsBackstagePasses()
+                .Build();
+
+            var startingQuality = tickets.Quality;
+
+            qualityService.UpdateItemQuality(tickets);
+
+
+            Assert.That(tickets.Quality, Is.EqualTo(startingQuality + 3));
+        }
+
+        [Test]
+        public void UpdateItemQuality_Tickets0DaysBeforeConcert_QualityIs0()
+        {
+            var qualityService = GetQualityService();
+
+            var tickets = ItemBuilder.DefaultItem()
+                .WithQualityOf(20)
+                .WithSellInOf(0)
+                .ItemAsBackstagePasses()
+                .Build();
+
+
+            qualityService.UpdateItemQuality(tickets);
+
+
+            Assert.That(tickets.Quality, Is.EqualTo(0));
+        }
+
+
     }
 }
