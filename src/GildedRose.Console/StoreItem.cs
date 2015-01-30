fd8b6239a848f15d64956fb432ef3b91605c79d3
Phil <peva0411@gmail.com>
2015-01-29 20:54:23 -0500

    added strategy

---
+++ src/GildedRose.Console/StoreItem.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GildedRose.Console
+{
+
+    public interface IUpdateQualityStrategy
+    {
+        void UpdateQuality(StoreItem storeItem);
+    }
+
+    public class BetterWithTimeUpdateQualityStrategy : IUpdateQualityStrategy
+    {
+        public void UpdateQuality(StoreItem storeItem)
+        {
+            if (storeItem.Quality < 50)
+            {
+                storeItem.Quality++;
+            }
+            storeItem.SellIn--;
+        }
+    }
+
+
+    public class StoreItem
+    {
+        private readonly Item _item;
+        private readonly IUpdateQualityStrategy _updateQualityStrategy;
+
+        public StoreItem()
+        {
+        }
+
+        public StoreItem(Item item)
+        {
+            _item = item;
+            if (Name == "Aged Brie")
+            {
+                _updateQualityStrategy = new BetterWithTimeUpdateQualityStrategy();
+            }
+        }
+
+        public string Name
+        {
+            get { return _item.Name; }
+            set { _item.Name = value; }
+        }
+
+        public int SellIn
+        {
+            get { return _item.SellIn; }
+            set { _item.SellIn = value; }
+        }
+
+        public int Quality
+        {
+            get { return _item.Quality; }
+            set { _item.Quality = value; }
+        }
+
+        public void UpdateItemQuality()
+        {
+
+            if (this.Name != "Aged Brie" && this.Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (this.Quality > 0)
+                {
+                    if (this.Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        this.Quality = this.Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (this.Quality < 50)
+                {
+                    this.Quality = this.Quality + 1;
+
+                    if (this.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (this.SellIn < 11)
+                        {
+                            if (this.Quality < 50)
+                            {
+                                this.Quality = this.Quality + 1;
+                            }
+                        }
+
+                        if (this.SellIn < 6)
+                        {
+                            if (this.Quality < 50)
+                            {
+                                this.Quality = this.Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (this.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                this.SellIn = this.SellIn - 1;
+            }
+
+            if (this.SellIn < 0)
+            {
+                if (this.Name != "Aged Brie")
+                {
+                    if (this.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (this.Quality > 0)
+                        {
+                            if (this.Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                this.Quality = this.Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        this.Quality = this.Quality - this.Quality;
+                    }
+                }
+                else
+                {
+                    if (this.Quality < 50)
+                    {
+                        this.Quality = this.Quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/GildedRose.Tests/Strategies/BetterWithTimeUpdateQualityStrategyTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace GildedRose.Tests.Strategies
+{
+
+
+    [TestFixture]
+    public class BetterWithTimeUpdateQualityStrategyTests : BaseStrategyTest
+    {
+        [Test]
+        public void UpdateItemQuality_AgedBrie_QualityShouldIncrease()
+        {
+
+            var agedBrie = ItemBuilder.DefaultItem()
+                .WithQualityOf(0)
+                .WithSellInOf(2)
+                .ItemAsAgedBrie()
+                .Build();
+
+            var startingQuality = agedBrie.Quality;
+
+            agedBrie.UpdateItemQuality();
+
+            Assert.That(agedBrie.Quality, Is.EqualTo(startingQuality + 1));
+        }
+
+        [Test]
+        public void UpdateItemQuality_AgedBrie_QualityNeverGreaterThan50()
+        {
+
+
+            var agedBrie = ItemBuilder.DefaultItem()
+                .WithQualityOf(SYSTEM_MAX_Quality)
+                .WithSellInOf(2)
+                .ItemAsAgedBrie()
+                .Build();
+
+            agedBrie.UpdateItemQuality();
+
+            Assert.That(agedBrie.Quality, Is.EqualTo(SYSTEM_MAX_Quality));
+        }
+
+        [Test]
+        public void UpdateItemQuality_AgedBrieAfterSellin_QualityIncreaseBy2()
+        {
+
+            var agedBrie = ItemBuilder.DefaultItem()
+                .WithQualityOf(0)
+                .WithSellInOf(0)
+                .ItemAsAgedBrie()
+                .Build();
+
+            var startingQuality = agedBrie.Quality;
+
+            agedBrie.UpdateItemQuality();
+
+            Assert.That(agedBrie.Quality, Is.EqualTo(startingQuality + 2));
+        }
+    }
+
+    public class BaseStrategyTest
+    {
+        protected int SYSTEM_MAX_Quality = 50;
+    }
+}
